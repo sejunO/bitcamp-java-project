0e768abd5d1ba51a89cc7c1cf589b9f239246e14
sejunO <frame8916@gmail.com>
2020-07-17 14:45:18 +0900

    mini-project

---
+++ src/main/java/com/eomcs/pms/APP.java
@@ -0,0 +1,13 @@
+package com.eomcs.pms;
+
+public class APP{
+    public static void main(String[] args){
+        System.out.println("[회원]");
+        System.out.println("이름: 홍길동");
+        System.out.println("이메일: hong@test.com");
+        System.out.println("암호: 1111");
+        System.out.println("사진: hong.png");
+        System.out.println("전화: 1111-2222");
+        System.out.println("가입일: 2020-01-01");
+    }
+}
